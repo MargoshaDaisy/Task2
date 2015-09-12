ae430584b0fe5dab66335c09f8bf4643ff547e29
fpmi <fpmi@mac-mini-xadmin.wifi.bsu>
2015-09-12 11:20:43 +0300

    Initial Commit

---
+++ KM_LV_Task2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KM_LV_Task2
+//
+//  Created by fpmi on 12.09.15.
+//  Copyright (c) 2015 fpmi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
